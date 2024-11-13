6cf8ccfb7ea077ff5f0fde87384b4adbf38f35fb
m.shamsipour <maziar56bnd@gmail.com>
2024-11-13 17:59:53 +0330

    [feat] Implement hybrid search functionality.

--- controllers/api_controller.py
+++ controllers/api_controller.py
@@ -1,18 +1,24 @@
+from typing import List, Optional, Dict
+
 from pydantic import ValidationError
 
 from controllers.qdrant_manager import QdrantManager
 from flask import request, jsonify
+
+from models.product import Product
 from models.query import Query, RetrievalType
-from loguru import logger
 
 
 class ApiController:
 
     def __init__(self,
-                 qdrant_configs):
+                 qdrant_configs,
+                 hybrid_search_configs: Optional[Dict] = None, ):
         text_index_configs = qdrant_configs.get('text_index_configs')
         if text_index_configs:
             text_index_name = text_index_configs.get('field_name')
+
+        self.hybrid_search_configs = hybrid_search_configs
         self.qdrant_manager = QdrantManager.get_qdrant_manager(url=qdrant_configs['db_url'],
                                                                api_key=qdrant_configs['db_api_key'],
                                                                collection_name=qdrant_configs['product_collection'],
@@ -26,7 +32,6 @@ class ApiController:
             retrieval_type = request_args.pop('retrieval_type', RetrievalType.hybrid)
             size = request_args.pop('size', 5)
             filters = request_args.to_dict()
-            logger.info(filters)
             query = Query(query=query,
                           retrieval_type=retrieval_type,
                           size=size,
@@ -40,7 +45,8 @@ class ApiController:
         elif query.retrieval_type == RetrievalType.keyword:
             return self.__keyword_search(query)
         elif query.retrieval_type == RetrievalType.hybrid:
-            return self.__hybrid_search(query)
+            semantic_results_percent = self.hybrid_search_configs.get('semantic_results_percent', 50)
+            return self.__hybrid_search(query, semantic_results_percent)
         else:
             return jsonify({'description': 'Unsupported query type'}), 501
 
@@ -56,5 +62,37 @@ class ApiController:
                                                                  query_filter=query.filters)
         return jsonify([r['product'].to_response_obj() for r in results]), 200
 
-    def __hybrid_search(self, query: Query):
-        pass
+    def __hybrid_search(self, query: Query, semantic_result_percentage: int = 50):
+        semantic_results = self.qdrant_manager.search_products_by_text(text=query.query,
+                                                                       top_k=query.size,
+                                                                       query_filter=query.filters)
+        semantic_results = [res['product'] for res in semantic_results]
+
+        keyword_results = self.qdrant_manager.search_products_by_keyword(text=query.query,
+                                                                         top_k=query.size,
+                                                                         query_filter=query.filters)
+        keyword_results = [res['product'] for res in keyword_results]
+
+        # combine the results
+        results = semantic_results[:int(query.size * semantic_result_percentage / 100)]
+
+        for res in keyword_results:
+            if len(results) >= query.size:
+                break
+            if self.__not_duplicated(res, results):
+                results.append(res)
+
+        for res in semantic_results[int(query.size * semantic_result_percentage / 100):]:
+            if len(results) >= query.size:
+                break
+            if self.__not_duplicated(res, results):
+                results.append(res)
+
+        return jsonify([r.to_response_obj() for r in results]), 200
+
+    @staticmethod
+    def __not_duplicated(product: Product, product_list: List[Product]):
+        for product_item in product_list:
+            if product_item.uuid == product.uuid:
+                return False
+        return True
